b43718a530ca6ad9027da4b63323edb2bebf47ba
AngeloGarcia <guioangelo.garcia@gmail.com>
2024-06-07 11:50:29 -0500

    primer commit

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_AngeloGarcia/DaaiiT1AngeloGarciaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_AngeloGarcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaaiiT1AngeloGarciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaaiiT1AngeloGarciaApplication.class, args);
+	}
+
+}
